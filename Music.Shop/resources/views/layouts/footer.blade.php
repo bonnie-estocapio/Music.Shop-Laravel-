60f9d1ee933e7df22a01070981c15f0637ec99e8
bonnie-estocapio <134898783+bonnie-estocapio@users.noreply.github.com>
2023-07-13 14:58:11 +0800

    feat: added header, footer, and master layout

---
+++ Music.Shop/resources/views/layouts/footer.blade.php
@@ -0,0 +1,13 @@
+<footer class="bg-light text-center text-lg-start" style="position: fixed; width: 100%;bottom: 0;">          
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      © 2023 Copyright:
+      <a class="text-dark" href="">Music Locker</a>
+    </div>
+    <!-- Copyright -->
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
